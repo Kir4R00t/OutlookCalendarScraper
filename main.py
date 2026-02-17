8a4d431ed19900e8ac2e139452b85a47c535e2ba
Kir4R00t <przemyslaw.sadowski2004@gmail.com>
2026-02-18 00:41:38 +0100

    updating req file & fixing import bug

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.common.by import By, Keys
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
 from selenium.common.exceptions import (
     StaleElementReferenceException, 
     TimeoutException
