d87d97c769635127c3c529d17a9a6a7cc75058f2
Kir4R00t-thinkpad <55001500+Kir4R00t@users.noreply.github.com>
2026-02-17 12:25:25 +0100

    added function that finds all elements

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
 from selenium import webdriver
 
 from dotenv import load_dotenv # type: ignore
@@ -20,13 +22,19 @@ class CalendarScraper:
         driver = webdriver.Chrome(options=options)
         return driver
 
-    def find
-
+    # TODO: finds all events but in the current month
+    def find_all_events(self, driver, timeout: int = 15):
+        wait = WebDriverWait(driver, timeout)
+        wait.until(EC.presence_of_all_elements_located((By.CSS_SELECTOR, "div[data-itemindex]")))
+        
+        return driver.find_elements(By.CSS_SELECTOR, "div[data-itemindex]")
+    
     def run(self):
         driver = self.init_driver()
         driver.get(self.url)
         
-        print(driver.title)
+        events = self.find_all_events(driver)
+        
         driver.quit()
 
 if __name__ == "__main__":
